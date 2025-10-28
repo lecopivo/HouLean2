8c1ce5e925d6d7627db3760cd3cf2f4e55aa538f
Tomas Skrivan <skrivantomas@seznam.cz>
2025-10-28 15:50:29 +0100

    ...

---
+++ HouLean.lean
@@ -0,0 +1,11 @@
+-- This module serves as the root of the `HouLean` library.
+-- Import modules here that should be built as part of the library.
+import HouLean.Math
+import HouLean.LinearAlgebra
+import HouLean.Data.Vector3
+import HouLean.Apex.Basic
+import HouLean.Apex.Data.Float
+import HouLean.Apex.Data.Vector3
+-- import HouLean.Apex.Data
+-- import HouLean.Apex.Geometry
+
