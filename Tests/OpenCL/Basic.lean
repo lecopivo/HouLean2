04e1c93b24cfeaa0e5278ba6f4916bc6e90f4b48
Tomas Skrivan <skrivantomas@seznam.cz>
2025-12-18 04:18:50 +0100

    ...

--- Tests/OpenCL/Basic.lean
+++ Tests/OpenCL/Basic.lean
@@ -9,19 +9,12 @@ open HouLean OpenCL
 namespace Tests.OpenCL.Basic
 
 
-#exit
-/--
-info:
-float houlean_math_lerp_ff(float x, float y, float t)
-{
-    return x + ((y - x) * t);
-}
 
-float (anonymous)(float x, float y)
-{
-    float a = x * y;
-    float b = houlean_math_lerp_ff(x, y, 0.300000011920929f);
-    return a + b;
+/--
+info: float main(float x, float y){
+      const float a = x * y;
+      const float b = mix(x, y, 0.3);
+      return a + b;
 }
 -/
 #guard_msgs in
@@ -31,22 +24,10 @@ float (anonymous)(float x, float y)
   a + b)
 
 /--
-info:
-float3 vector_lerp_f3(float3 x, float3 y, float t)
-{
-    return x + (t * (y - x));
-}
-
-float3 houlean_math_lerp_f3f(float3 x, float3 y, float t)
-{
-    return vector_lerp_f3(x, y, t);
-}
-
-float3 (anonymous)(float3 x, float3 y)
-{
-    float3 a = x + y;
-    float3 b = houlean_math_lerp_f3f(x, y, 0.300000011920929f);
-    return a + b;
+info: float3 main(float3 x, float3 y){
+      const float3 a = x + y;
+      const float3 b = mix(x, y, 0.3);
+      return a + b;
 }
 -/
 #guard_msgs in
@@ -56,7 +37,56 @@ float3 (anonymous)(float3 x, float3 y)
   a + b)
 
 
-/-- error: Not an OpenCL type Matrix Float32 3 3! -/
+/--
+info: matrix33float houlean_matrix_add_float32_3_3(matrix33float a, matrix33float b){
+      return (matrix33float){a.row0 + b.row0, a.row1 + b.row1, a.row2 + b.row2};
+}
+
+matrix33float add_add_matrix_float32_3_3(matrix33float a, matrix33float a1){
+      return houlean_matrix_add_float32_3_3(a, a1);
+}
+
+matrix33float hadd_hadd_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(matrix33float a, matrix33float a1){
+      return add_add_matrix_float32_3_3(a, a1);
+}
+
+matrix33float houlean_matrix_sub_float32_3_3(matrix33float a, matrix33float b){
+      return (matrix33float){a.row0 - b.row0, a.row1 - b.row1, a.row2 - b.row2};
+}
+
+matrix33float sub_sub_matrix_float32_3_3(matrix33float a, matrix33float a1){
+      return houlean_matrix_sub_float32_3_3(a, a1);
+}
+
+matrix33float hsub_hsub_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(matrix33float a, matrix33float a1){
+      return sub_sub_matrix_float32_3_3(a, a1);
+}
+
+matrix33float houlean_matrix_smul_float32_3_3_0_3(matrix33float a){
+      return (matrix33float){0.3 * a.row0, 0.3 * a.row1, 0.3 * a.row2};
+}
+
+matrix33float hmul_hmul_float32_matrix_float32_3_3_matrix_float32_3_3_0_3(matrix33float a){
+      return houlean_matrix_smul_float32_3_3_0_3(a);
+}
+
+matrix33float houlean_matrix_lerp_float32_3_3_0_3(matrix33float a, matrix33float b){
+      return
+        hadd_hadd_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(a,
+          hmul_hmul_float32_matrix_float32_3_3_matrix_float32_3_3_0_3(hsub_hsub_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(b,
+              a)));
+}
+
+matrix33float houlean_math_lerp_lerp_matrix_float32_3_3_float32_0_3(matrix33float x, matrix33float y){
+      return houlean_matrix_lerp_float32_3_3_0_3(x, y);
+}
+
+matrix33float main(matrix33float x, matrix33float y){
+      const matrix33float a = hadd_hadd_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(x, y);
+      const matrix33float b = houlean_math_lerp_lerp_matrix_float32_3_3_float32_0_3(x, y);
+      return hadd_hadd_matrix_float32_3_3_matrix_float32_3_3_matrix_float32_3_3(a, b);
+}
+-/
 #guard_msgs in
 #opencl_compile (fun x y : Matrix Float32 3 3 =>
   let a := x + y
@@ -72,16 +102,13 @@ def foo (n : Nat) (x : Foo n) : Float :=
   a
 
 /--
-info:
-double tests_opencl_basic_foo(uint n, double x)
-{
-    double a = x + x;
-    return a;
+info: double tests_opencl_basic_foo1(uint n, double x){
+      const double a = x + x;
+      return a;
 }
 
-double (anonymous)(uint n, double x)
-{
-    return tests_opencl_basic_foo(n, x);
+double main(uint n, double x){
+      return tests_opencl_basic_foo1(n, x);
 }
 -/
 #guard_msgs in
