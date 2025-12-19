f234720af9676dee9ec6f9f026fe8bda4ea22cd9
Tomas Skrivan <skrivantomas@seznam.cz>
2025-12-19 01:17:34 +0100

    ...

--- HouLean/OpenCL/Data/NanoVDB.lean
+++ HouLean/OpenCL/Data/NanoVDB.lean
@@ -1,9 +1,11 @@
-#exit
 import HouLean.Math
 import HouLean.Data.Defs
 import HouLean.Data.Float
 import HouLean.Data.Vector
 import HouLean.OpenCL.Basic
+import HouLean.OpenCL.Data.Vector
+import HouLean.OpenCL.Data.Pointer
+import HouLean.Meta.DoNotation
 
 namespace HouLean.OpenCL
 
@@ -12,78 +14,128 @@ abbrev NanoVDB.Coord := Vec3 Int32
 abbrev NanoVDB.Vec3F := Vec3 Float32
 abbrev NanoVDB.Vec3D := Vec3 Float64
 
-class NanoVDB.ValueType (T : Type) where
-  suffix : String
-  -- allowed types
-  valid : T = Float32 ∨
-          T = Vec3 Float32 ∨
-          T = Int32 := by simp
+-- class NanoVDB.ValueType (T : Type) where
+--   suffix : String
+--   -- allowed types
+--   valid : T = Float32 ∨
+--           T = Vec3 Float32 ∨
+--           T = Int32 := by simp
 
-/-- Represents a pointer to NanoVDB grid with values of type `α` -/
-opaque NanoVDB (T : Type) [NanoVDB.ValueType T] : Type
+-- /-- Represents a pointer to NanoVDB grid with values of type `α` -/
+abbrev NanoVDB (T : Type) (const : Bool := false) := DPointer Unit (addr:=.global) (const:=const) (restrict:=true)
 
 namespace NanoVDB
 
-instance [t : ValueType α] : OpenCLType (NanoVDB α) where
-  name := "void *"
-  shortName := s!"vdb{t.suffix}"
+-- instance [t : ValueType α] : OpenCLType (NanoVDB α) where
+--   name := "void *"
+--   shortName := s!"vdb{t.suffix}"
 
-instance [Inhabited T] : Inhabited (Vec3 T) := ⟨⟨default⟩⟩
+-- instance [Inhabited T] : Inhabited (Vec3 T) := ⟨⟨default⟩⟩
 
-instance : ValueType (Float32) where
-  suffix := "F"
+-- instance : ValueType (Float32) where
+--   suffix := "F"
 
-instance : ValueType (Vec3 Float32) where
-  suffix := "F3"
+-- instance : ValueType (Vec3 Float32) where
+--   suffix := "F3"
 
-instance : ValueType Int32 where
-  suffix := "I"
+-- instance : ValueType Int32 where
+--   suffix := "I"
 
-instance : OpenCLType (Vec3 Float32) where
-  name := "cnanovdb_Vec3F"
-  shortName := "F3"
+-- instance : OpenCLType (Vec3 Float32) where
+--   name := "cnanovdb_Vec3F"
+--   shortName := "F3"
 
-instance : OpenCLType Coord where
-  name := "cnanovdb_coord"
-  shortName := "I3"
+-- instance : OpenCLType Coord where
+--   name := "cnanovdb_coord"
+--   shortName := "I3"
 
-noncomputable section
+-- noncomputable section
 
-abbrev vec3FToVector : Vec3 Float32 → Vector Float32 3 := oclFunction _ "cnanovdb_Vec3F_to_float3"
-abbrev vectorToVec3F : Vector Float32 3 → Vec3 Float32 := oclFunction _ "cnanovdb_float3_to_Vec3F"
-abbrev coordToVector : Coord → Vector Int32 3 := oclFunction _ "cnanovdb_coord_to_int3"
-abbrev vectorToCoord : Vector Int32 3 → Coord := oclFunction _ "cnanovdb_int3_to_coord"
+-- abbrev vec3FToVector : Vec3 Float32 → Vector Float32 3 := oclFunction _ "cnanovdb_Vec3F_to_float3"
+-- abbrev vectorToVec3F : Vector Float32 3 → Vec3 Float32 := oclFunction _ "cnanovdb_float3_to_Vec3F"
+-- abbrev coordToVector : Coord → Vector Int32 3 := oclFunction _ "cnanovdb_coord_to_int3"
+-- abbrev vectorToCoord : Vector Int32 3 → Coord := oclFunction _ "cnanovdb_int3_to_coord"
 
-instance : Coe (Vec3 Float32) (Vector Float32 3) := ⟨vec3FToVector⟩
-instance : Coe (Vector Float32 3) (Vec3 Float32) := ⟨vectorToVec3F⟩
-instance : Coe Coord (Vector Int32 3) := ⟨coordToVector⟩
-instance : Coe (Vector Int32 3) Coord  := ⟨vectorToCoord⟩
+-- instance : Coe (Vec3 Float32) (Vector Float32 3) := ⟨vec3FToVector⟩
+-- instance : Coe (Vector Float32 3) (Vec3 Float32) := ⟨vectorToVec3F⟩
+-- instance : Coe Coord (Vector Int32 3) := ⟨coordToVector⟩
+-- instance : Coe (Vector Int32 3) Coord  := ⟨vectorToCoord⟩
 
 
 -- cnanovdb_readaccessor
-opaque ReadAccessor (T : Type) [ValueType T] : Type := Unit
-instance [ValueType T] : Inhabited (ReadAccessor T) := ⟨cast sorry_proof ()⟩
+opaque ReadAccessor : Type := Unit
+impl_type_by ReadAccessor ==> cnanovdb_readaccessor
 
--- I think this type should have pointer semantics the same as NanoVDB
---
-instance [ValueType T]: OpenCLType (ReadAccessor T) where
-  name := "cnanovdb_readaccessor"
-  shortName := "cnanovdb_readaccessor"
+abbrev ReadAccessorPtr (T : Type) := DPointer ReadAccessor (addr:=.default) (const:=false)
 
 
-variable {T} [vt : ValueType T]
+-- @[opencl_extern "cnanovdb_getreadaccessor"]
+opaque getReadAccessor (grid : NanoVDB T) (acc : ReadAccessorPtr T) : OpenCLM Unit
+impl_by {T} (grid : NanoVDB T) (acc : ReadAccessorPtr T) :
+  getReadAccessor grid acc ==> cnanovdb_getreadaccessor(grid, acc)
 
--- todo: I should somehow specify the calling sematics is:
---          void(const global void * grid, cnanovdb_readaccessor * acc)
---       and not
---          cnanovdb_readaccessor(const global void * grid)
-abbrev getReadAccessor : (grid : NanoVDB T) → OpenCLM (ReadAccessor T) :=
-  oclFunction _ "cnanovdb_getreadaccessor"
+noncomputable
+opaque getReadAccessor' (grid : NanoVDB T) : OpenCLM (ReadAccessorPtr T)
+impl_by {T} (grid : NanoVDB T) :
+  getReadAccessor' grid ==> cnanovdb_getreadaccessor(grid)
 
-set_option linter.unusedVariables false
 
-abbrev ReadAccessor.isActive : (acc : ReadAccessor T) → (coord : Coord) → OpenCLM Bool :=
-  oclFunction _ s!"cnanovdb_readaccessor_isActive{vt.suffix}"
+open Lean Meta Compiler Qq
+def vdbTypeSuffix (t : Expr) : MetaM String := do
+  if ← isDefEq t q(Float32) then
+    return "F"
+  else if ← isDefEq t q(Vector Float32 3) then
+    return "V3"
+  else
+    throwError "not a valid value type!"
 
-abbrev ReadAccessor.set : (acc : ReadAccessor T) → (coord : Coord) → (value : T) → OpenCLM Bool :=
-  oclFunction _ s!"cnanovdb_readaccessor_setValue{vt.suffix}"
+
+opaque ReadAccessorPtr.isActive (acc : ReadAccessorPtr T) (coord : Vector Int32 3) : OpenCLM Bool
+impl_by {T} (acc : ReadAccessorPtr T) (coord : Vector Int32 3) :
+    acc.isActive coord ==> do
+  let acc ← compileExpr acc
+  let coord ← compileExpr coord
+  let suffix ← vdbTypeSuffix T
+  let funId := mkIdent <| Name.appendAfter `cnanovdb_readaccessor_isActive suffix
+  return ← `(clExpr| $funId:ident($acc, $coord))
+
+
+opaque ReadAccessorPtr.get {T} [Inhabited T] (acc : ReadAccessorPtr T) (coord : Vector Int32 3) : OpenCLM T
+impl_by {T} [Inhabited T] (acc : ReadAccessorPtr T) (coord : Vector Int32 3) :
+    acc.get coord ==> do
+  let acc ← compileExpr acc
+  let coord ← compileExpr coord
+  let suffix ← vdbTypeSuffix T
+  let funId := mkIdent <| Name.appendAfter `cnanovdb_readaccessor_getValue suffix
+  return ← `(clExpr| $funId:ident($acc, $coord))
+
+
+opaque ReadAccessorPtr.set (acc : ReadAccessorPtr T) (coord : Vector Int32 3) (value : T) : OpenCLM Unit
+impl_by {T} (acc : ReadAccessorPtr T) (coord : Vector Int32 3) (value : T) :
+    acc.set coord value ==> do
+  let acc ← compileExpr acc
+  let coord ← compileExpr coord
+  let val ← compileExpr value
+  let suffix ← vdbTypeSuffix T
+  let funId := mkIdent <| Name.appendAfter `cnanovdb_readaccessor_setValue suffix
+  return ← `(clExpr| $funId:ident($acc, $coord, $val))
+
+
+
+-- attribute [opencl_csimp] Id.run
+-- impl_by {α : Type} (a : α) : ForInStep.yield a ==> a
+
+
+-- #opencl_compile (fun (acc : ReadAccessorPtr (Vector Float32 3)) => do
+--   let v ← acc.get #v[0,0,0]
+--   let u ← acc.get #v[1,0,0]
+--   acc.set #v[2,0,0] (u+v)
+
+--   let mut s := 0
+--   for i in [0:10] do
+--     for j in [0:10] do
+--       for k in [0:10] do
+--         let v ← acc.get #v[i.toInt32,j.toInt32,k.toInt32]
+--         s += v
+
+--   return s)
--- Tests/OpenCL/MPM.lean
+++ Tests/OpenCL/MPM.lean
@@ -1,76 +1,70 @@
-#exit
 import HouLean.OpenCL.Data.Matrix
-import HouLean.OpenCL.Data.ArrayRef
+import HouLean.OpenCL.Data.Pointer
+-- import HouLean.OpenCL.Data.ArrayRef
 import HouLean.OpenCL.Data.NanoVDB
-import HouLean.OpenCL.Data.Unit
-import HouLean.OpenCL.Data.Int
-import HouLean.OpenCL.Data.Bool
-import HouLean.OpenCL.WorkItemFunctions
-import HouLean.OpenCL.Data.ArrayType
+-- import HouLean.OpenCL.Data.Unit
+-- import HouLean.OpenCL.Data.Int
+-- import HouLean.OpenCL.Data.Bool
+-- import HouLean.OpenCL.WorkItemFunctions
+-- import HouLean.OpenCL.Data.ArrayType
 
 open HouLean OpenCL
 
 namespace Test.OpenCL.MPM
 
 
-
-noncomputable
-def clearSurfaces
-    (tilestartsxyx : ArrayPointer (Vector Int32 3)) -- missing global, const, restric
-    (surface_grid : NanoVDB Float32)       -- missing global, restric
-    (surface_grid_out : NanoVDB Float32)   -- missing global, restric
-    : OpenCLM Unit := do
-
-  let surface_grid_acc ← surface_grid.getReadAccessor
-  let surface_grid_out_acc ← surface_grid_out.getReadAccessor
-
+def getGridIdx (tilestartsxyx : DPointer (Vector Int32 3)) : OpenCLM (Vector Int32 3) := do
   let idx ← getGlobalId 0
 
   let tileoffset := idx &&& 511
   let tileidx := idx >>> 9
-  let tilebasexyz ← tilestartsxyx[tileidx.toUInt64]
+  let tilebasexyz ← tilestartsxyx.get tileidx
 
-  let gidx := tilebasexyz[0] + (tileoffset &&& 7).toInt32
-  let gidy := tilebasexyz[1] + ((tileoffset >>> 3) &&& 7).toInt32
-  let gidz := tilebasexyz[2] + ((tileoffset >>> 6) &&& 7).toInt32
+  let gidx := tilebasexyz[0] + (tileoffset &&& 7).toUInt32.toInt32
+  let gidy := tilebasexyz[1] + ((tileoffset >>> 3) &&& 7).toUInt32.toInt32
+  let gidz := tilebasexyz[2] + ((tileoffset >>> 6) &&& 7).toUInt32.toInt32
 
-  let g_coord : NanoVDB.Coord := #v[gidx, gidy, gidz]
+  return #v[gidx, gidy, gidz]
+
+-- #opencl_compile getGridIdx
+
+noncomputable
+def clearSurfaces
+    (tilestartsxyx : DPointer (Vector Int32 3)) -- missing global, const, restric
+    (surface_grid : NanoVDB Float32)       -- missing global, restric
+    (surface_grid_out : NanoVDB Float32)   -- missing global, restric
+    : OpenCLM Unit := do
+
+  let surface_grid_acc ← surface_grid.getReadAccessor'
+  let surface_grid_out_acc ← surface_grid_out.getReadAccessor'
+
+  let g_coord ← getGridIdx tilestartsxyx
   let active ← surface_grid_acc.isActive g_coord
   if !active then
     return
 
-  let _ ← surface_grid_acc.set g_coord 0
-  let _ ← surface_grid_out_acc.set g_coord 0
+  surface_grid_acc.set g_coord 0
+  surface_grid_out_acc.set g_coord 0
   return
 
 -- #opencl_compile clearSurfaces
 
 noncomputable
 def clearGrids
-    (tilestartsxyx : ArrayPointer (Vector Int32 3))  -- missing global, const, restric
+    (tilestartsxyx : DPointer (Vector Int32 3))  -- missing global, const, restric
     (mass_grid : NanoVDB Float32)                    -- missing global, restric
-    (vel_grid : NanoVDB (NanoVDB.Vec3 Float32))      -- missing global, restric
+    (vel_grid : NanoVDB (Vector Float32 3))      -- missing global, restric
     : OpenCLM Unit := do
 
-  let mass_grid_acc ← mass_grid.getReadAccessor
-  let vel_grid_acc ← vel_grid.getReadAccessor
+  let mass_grid_acc ← mass_grid.getReadAccessor'
+  let vel_grid_acc ← vel_grid.getReadAccessor'
 
-  let idx ← getGlobalId 0
-
-  let tileoffset := idx &&& 511
-  let tileidx := idx >>> 9
-  let tilebasexyz ← tilestartsxyx[tileidx.toUInt64]
-
-  let gidx := tilebasexyz[0] + (tileoffset &&& 7).toInt32
-  let gidy := tilebasexyz[1] + ((tileoffset >>> 3) &&& 7).toInt32
-  let gidz := tilebasexyz[2] + ((tileoffset >>> 6) &&& 7).toInt32
-
-  let g_coord : NanoVDB.Coord := #v[gidx, gidy, gidz]
+  let g_coord ← getGridIdx tilestartsxyx
   if !(← mass_grid_acc.isActive g_coord) then
     return
 
-  let _ ← mass_grid_acc.set g_coord 0
-  let _ ← vel_grid_acc.set g_coord (0 : Vector Float32 3)
+  mass_grid_acc.set g_coord 0
+  vel_grid_acc.set g_coord (0 : Vector Float32 3)
   return
 
 -- #opencl_compile clearGrids
